7d41c5020fd59de879594fd3cd1e8fcaa1ed8f4d
henrietta-k <111554249+henrietta-k@users.noreply.github.com>
2023-06-26 16:25:38 +0800

    Update MainGame.java

--- MainGame.java
+++ MainGame.java
@@ -1,14 +1,8 @@
 public class MainGame {
 
     public static void main(String[] args) { 
-
         static Battleship myMap = new Battleship(); //creating new Maze called myMap 
 
         runBattleShipGame(); 
-
-    }
- 
-
-
-    
+    } 
 }
